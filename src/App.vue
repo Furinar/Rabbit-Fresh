08202853ec134b9967bab0326dbc0c6048bf5175
Furina <3559551198@qq.com>
2025-12-19 19:30:28 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+    <h1>Welcome to RabbitShop</h1>
+  </div>
+</template>
+
+<script setup lang="ts" name="App">
+import { ref, reactive } from 'vue'
+
+
+</script>
+
+<style scoped></style>
