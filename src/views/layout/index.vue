10d9218e54c80c3efa8ca023a8885843f3138861
Furina <3559551198@qq.com>
2025-12-23 15:34:33 +0800

    实现header吸顶

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,11 +1,14 @@
 <template>
+  <layoutFixed />
   <layoutNav />
   <layoutHeader />
   <RouterView />
+  <div style="height: 100px;"></div>
   <layoutFooter />
 </template>
 
 <script setup lang="ts">
+import layoutFixed from './components/layoutFixed.vue';
 import layoutFooter from './components/layoutFooter.vue';
 import layoutNav from './components/layoutNav.vue';
 import layoutHeader from './components/layoutHeader.vue';
